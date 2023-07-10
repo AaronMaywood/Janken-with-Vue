14a74882d26fcf18d4bca4b32a1c925623b032cb
Aaron Maywood <am7274183@gmail.com>
2023-07-10 14:50:30 +0900

    ユーザー手の入力可能なコンポーネントの実装

---
+++ src/components/UserHand.vue
@@ -0,0 +1,19 @@
+<script setup>
+const emit = defineEmits(['change'])
+
+function updateUserHand(e){
+	emit('change',e.target.value)
+}
+</script>
+<template>
+	<div>
+		<label><input type="radio" name="hand" value="👊" v-on:click="updateUserHand"><img src="images/janken_gu.png" width="64" alt=""></label>
+		<label><input type="radio" name="hand" value="✌" v-on:click="updateUserHand"><img src="images/janken_choki.png" width="64" alt=""></label>
+		<label><input type="radio" name="hand" value="✋" v-on:click="updateUserHand"><img src="images/janken_pa.png" width="64" alt=""></label>
+	</div>
+</template>
+<style scoped>
+input[type="radio"] {
+	display: none;
+}
+</style>
