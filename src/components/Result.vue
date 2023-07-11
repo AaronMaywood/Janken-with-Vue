e668515a627c5a974768a7304695c387881eff7a
Aaron Maywood <am7274183@gmail.com>
2023-07-11 13:08:06 +0900

    じゃんけんの試合の結果を表示するようにした

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,18 +1,80 @@
 <script setup>
+import { ref,computed } from 'vue'
+const play = ref("")
+const judge = ref("")
+
 const props = defineProps({
   show: Boolean,
   user: "",
 })
+
+const hand = ['👊','✌','✋']
+const hand_img = [
+  '<img src="images/janken_gu.png" width="64" alt="">',
+  '<img src="images/janken_choki.png" width="64" alt="">',
+  '<img src="images/janken_pa.png" width="64" alt="">'
+]
+
+const result = computed(() => {
+  return props.show ? judge_play() : { play: '', judge: '' }
+})
+
+function judge_play(){
+  let user_win = 0, comp_win = 0
+  const comp_index = Math.floor(Math.random()*3)
+  const comp = hand[comp_index]
+
+  let judge
+  if( props.user === comp ){
+    judge = 'あいこ'
+  }else if( props.user === '👊' && comp === '✌'){
+    judge = 'あなたの勝ち！'
+    user_win++
+  }else if( props.user === '👊' && comp === '✋'){
+    judge = 'コンピューターの勝ち！'
+    comp_win++
+  }else if( props.user === '✌' && comp === '👊'){
+    judge = 'コンピューターの勝ち！'
+    comp_win++
+  }else if( props.user === '✌' && comp === '✋'){
+    judge = 'あなたの勝ち！'
+    user_win++
+  }else if( props.user === '✋' && comp === '👊'){
+    judge = 'あなたの勝ち！'
+    user_win++
+  }else if( props.user === '✋' && comp === '✌'){
+    judge = 'コンピューターの勝ち！'
+    comp_win++
+  }
+
+  const hand_to_index = {
+    '👊': 0,
+    '✌': 1,
+    '✋': 2
+  }
+  const user_hand = hand_img[hand_to_index[props.user]]
+  return {
+    play: `あなた:${user_hand}<br>コンピューター:${hand_img[comp_index]}`,
+    judge: judge,
+  }
+}
+
+
 </script>
 <template>
+  
 	<div id="result" v-if="show">
 		<p>じゃんけん...ぽん！</p>
     <p id="play">
-    <span v-if="user === '👊'"><img src="images/janken_gu.png" width="64" alt=""></span>
-    <span v-else-if="user === '✌'"><img src="images/janken_choki.png" width="64" alt=""></span>
-    <span v-else-if="user === '✋'"><img src="images/janken_pa.png" width="64" alt=""></span>
+      <!--
+        htmlを含む文字列を表示するときはマスタッシュ構文の変わりに v-html を使用する
+        https://ja.vuejs.org/guide/essentials/template-syntax.html#raw-html
+      -->
+      <span v-html="result.play"></span>
     </p>
-		<p id="judge"></p>
+    <p id="judge">
+      <span v-html="result.judge"></span>
+    </p>
 		<div id="restart_button">
 			<hr>
 			<button type="button" id="restart" @click="$emit('next')">再チャレンジ</button>
